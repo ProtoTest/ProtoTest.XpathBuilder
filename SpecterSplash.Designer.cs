a840e7d2a45b2fcfb48c5703c828295228eaf7d6
Seth Urban <surban@prototest.com>
2014-01-24 23:55:20 -0700

    Higher Rez Support
    Edited splash screen to render correctly on higher resolution monitors.

--- SpecterSplash.Designer.cs
+++ SpecterSplash.Designer.cs
@@ -42,22 +42,26 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(107, 396);
+            this.progressBar1.Location = new System.Drawing.Point(145, 398);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(288, 28);
+            this.progressBar1.Size = new System.Drawing.Size(216, 23);
             this.progressBar1.TabIndex = 1;
             // 
             // SpecterSplash
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(500, 470);
+            this.ClientSize = new System.Drawing.Size(500, 471);
             this.Controls.Add(this.progressBar1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "SpecterSplash";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SpecterSplash";
